f94a95e4a3c1d4c6483181eddf511d40688a3548
Familia Segolin <familiasegolin@gmail.com>
2025-04-06 21:42:35 +0900

    top bar red

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     topBarHack.style.backgroundColor = '#000';  // Cor para o modo escuro
   } else {
     document.body.classList.add('light-mode');
-    topBarHack.style.backgroundColor = '#eec49b';  // Cor para o modo claro
+    topBarHack.style.backgroundColor = '#ff0000';  // Cor para o modo claro
   }
 
   toggleButton.addEventListener('click', () => {
@@ -22,11 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.toggle('light-mode', !isDarkMode);
 
     // Modificando a cor do #top-bar-hack diretamente no JavaScript
-    //if (isDarkMode) {
-     // topBarHack.style.backgroundColor = '#000';  // Cor para o modo escuro
-    //} else {
-     // topBarHack.style.backgroundColor = '#eec49b';  // Cor para o modo claro
-    //}
+    if (isDarkMode) {
+      topBarHack.style.backgroundColor = '#000';  // Cor para o modo escuro
+    } else {
+      topBarHack.style.backgroundColor = '#eec49b';  // Cor para o modo claro
+    }
 
     // Salva a escolha do tema no localStorage
     localStorage.setItem('dark-mode', isDarkMode);
